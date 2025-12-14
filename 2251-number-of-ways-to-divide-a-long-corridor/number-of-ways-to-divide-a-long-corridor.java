640f023099ada632fa1297e332c295688f161dc9
kalyan joshi <139383788+joshi009@users.noreply.github.com>
2025-12-14 21:50:31 +0530

    Time: 17 ms (98.40%) | Memory: 47.1 MB (68.00%) - LeetSync

---
+++ 2251-number-of-ways-to-divide-a-long-corridor/number-of-ways-to-divide-a-long-corridor.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numberOfWays(String corridor) {
+        // step 1 - find all Seats, `S` indices
+        int n = corridor.length();
+        int[] seatIndices = new int[n];
+        int seatCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (corridor.charAt(i) == 'S') {
+                seatIndices[seatCount++] = i;
+            }
+        }
+
+        // step 2 - if seatCount is odd or zero, return 0
+        if (seatCount == 0 || seatCount % 2 != 0) {
+            return 0;
+        }
+
+        // step 3 - calculate ways to divide
+        long ways = 1;
+        int mod = 1_000_000_007;
+        int previousSeatIndex = seatIndices[1];
+        for (int i = 2; i < seatCount; i+=2){
+            // find partitions size between pairs of seats
+            int partitionSize = seatIndices[i] - previousSeatIndex;
+            
+            // update ways
+            ways = (ways * partitionSize) % mod;
+
+            // update previousSeatIndex
+            previousSeatIndex = seatIndices[i + 1];
+        }
+
+        // step 4 - return ways
+        return (int) ways;
+    }
+}
