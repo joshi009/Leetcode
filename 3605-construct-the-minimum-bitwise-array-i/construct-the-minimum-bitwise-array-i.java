fa282c24422d181c202ce19e6cfadbf49010ddb5
kalyan joshi <139383788+joshi009@users.noreply.github.com>
2026-01-20 21:11:01 +0530

    Time: 2 ms (65.79%) | Memory: 46.9 MB (30.70%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
@@ -1,22 +1,34 @@
 class Solution {
     public int[] minBitwiseArray(List<Integer> nums) {
-        int n = nums.size();
-        int[] ans = new int[n];
+        int[] ans = new int[nums.size()];
+        Arrays.fill(ans, -1);
 
-        for (int i = 0; i < n; i++) {
-            ans[i]=-1;
-            if(nums.get(i)==2){
-                ans[i]=-1;
+        for (int i = 0; i < ans.length; i++) {
+            int temp_1 = nums.get(i);
+            int min = Integer.MAX_VALUE;
+            if (((temp_1 + 1) % temp_1) != 0) {
+                for (int j = 0; j < 32; j++) {
+                    if ((temp_1 & (1 << j)) != 0) {
+                        int temp_2 = (temp_1 & ~(1 << j));
+                        if ((temp_2 | temp_2 + 1) == temp_1) {
+                            min = Math.min(min, temp_2);
+                        }
+                    }
+                }
+            if (min != Integer.MAX_VALUE) {
+                ans[i] = min;
             }
-            else{
-            for(int j=0;j<32;j++){
-                if((nums.get(i)&(1<<j))==0){
-                    ans[i]=nums.get(i)-(1<<(j-1));
-                    break;
+            
+            } else {
+                int n = temp_1;
+                int pos = -1;
+                while (n > 0) {
+                    pos++;
+                    n = n >> 1;
                 }
             }
-            }
         }
+
         return ans;
     }
 }
